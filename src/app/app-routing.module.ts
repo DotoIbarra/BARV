987ed12d5c750b791de1aad5922a79129f69f5ad
V1xIN <vi.verab@duocuc.cl>
2023-08-26 16:45:06 -0400

    nf2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,26 +3,38 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
+    path: 'folder/Inbox',
+    redirectTo: 'folder/Inbox',
     pathMatch: 'full'
   },
   {
-    path: 'pagina1',
-    loadChildren: () => import('./pages/pagina1/pagina1.module').then( m => m.Pagina1PageModule)
+    path: 'folder/:id',
+    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
   },
   {
-    path: 'pagina2',
-    loadChildren: () => import('./pages/pagina2/pagina2.module').then( m => m.Pagina2PageModule)
+    path: '',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'recuperacontra',
+    loadChildren: () => import('./pages/recuperacontra/recuperacontra.module').then( m => m.RecuperacontraPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'addauto',
+    loadChildren: () => import('./pages/addauto/addauto.module').then( m => m.AddautoPageModule)
+  },
+  {
+    path: 'addviaje',
+    loadChildren: () => import('./pages/addviaje/addviaje.module').then( m => m.AddviajePageModule)
   },
   {
     path: '**',
     loadChildren: () => import('./pages/not-found/not-found.module').then( m => m.NotFoundPageModule)
-  },
+  }
 ];
 
 @NgModule({
@@ -31,4 +43,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  public appPages = [
+    { title: 'Perfil', url: '/folder/inbox', icon: 'person-circle' },
+    { title: 'Editar Perfil', url: '/folder/archived', icon: 'build' },
+    { title: 'Reclamo', url: '/folder/spam', icon: 'alert-circle' },
+    { title: 'Cerrar Sesión', url: '/folder/trash', icon: 'exit' },
+  ];
+  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
